21565f22e7e3f03a896e1da81f7a9344c08166b8
cawfree <hello@cawfree.com>
2019-10-06 23:08:09 +0100

    Basic slide configuration.

--- 2019-10-25_ReactAdvanced_Parallelization/components/Layout.js
+++ 2019-10-25_ReactAdvanced_Parallelization/components/Layout.js
@@ -1,27 +1,77 @@
 import React from 'react'
+import Background from './../static/images/mapsy-flaticon-background.png';
+import Octo from './../static/images/octo.png';
+import LogoMini from './../static/images/logo_mini.svg';
+import LogoReact from './../static/images/logo-react.svg';
+import FlatIcon from './../static/images/flaticon.png';
 
 export default ({ children }) => (
-  <div
-    style={{
-      width: '100vw',
-      padding: '2vw',
-    }}
-  >
+  <>
+    <img
+      style={{
+        position: 'absolute',
+        top: 0,
+        width: '100vw',
+        height: '100vw',
+      }}
+      src={Background}
+    />
+    <img
+      style={{
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        opacity: 0.05,
+      }}
+      src={Octo}
+    />
     <div
       style={{
-        flex: 1,
+        position: 'absolute',
+        bottom: '0vw',
+        right: '1vw',
       }}
     >
-      {children}
+      <img
+        style={{
+          width: '5vw',
+          height: '5vw',
+          margin: '1vw',
+        }}
+        src={LogoReact}
+      />
+      <img
+        style={{
+          width: '5.5vw',
+          height: '5.5vw',
+          margin: '0.7vw',
+        }}
+        src={LogoMini}
+      />
+      <img
+        style={{
+          width: '11vw',
+          margin: '-1.8vw',
+        }}
+        src={FlatIcon}
+      />
     </div>
     <div
       style={{
         position: 'absolute',
-        bottom: '2vw',
+        top: 0,
         width: '100vw',
+        height: '100vw',
+        padding: '2vw',
       }}
-    >
-      <sub><sup><sub><sup>{'Alex Thomas (@cawfree) <hello@cawfree.com>'}</sup></sub></sup></sub>
+    > 
+      <div
+        style={{
+          flex: 1,
+        }}
+      >
+        {children}
+      </div> 
     </div>
-  </div>
+  </>
 );
